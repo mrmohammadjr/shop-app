5c7322ff284c1feaf601f37c6fd3416fc64efe97
mrmohammadjr <106073777+mrmohammadjr@users.noreply.github.com>
2025-06-05 19:59:24 +0330

    Update SearchBar.tsx

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -18,7 +18,7 @@ const SearchBar = () => {
   return (
     <div className="flex flex-col items-center">
       <div className="flex justify-between lg:my-16 sm:my-8 border-2 border-violet-600 w-[80%] items-center lg:p-7 sm:p-4 rounded-[5rem]">
-        <input type="text" onChange={(e)=> setSearch(e.target.value)} className="outline-none lg:text-3xl sm:text-sm sm:p-3 rounded-3xl w-full" placeholder="Search What You Want ..." />
+        <input type="text" onChange={(e)=> setSearch(e.target.value)} className="outline-none lg:text-2xl sm:text-sm sm:p-3 rounded-3xl w-full" placeholder="Search What You Want ..." />
         <i className="pi pi-search text-violet-500 lg:text-5xl sm:text-xl"></i>
       </div>
       {search.length > 0 ? (
